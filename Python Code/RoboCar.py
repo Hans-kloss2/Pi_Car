65ffa06f7c7fc8faa16aa924f825372861330e16
Erik M <hans_kloss2@t-online.de>
2022-11-14 13:07:58 +0100

    Test code

---
+++ Python Code/RoboCar.py
@@ -0,0 +1,18 @@
+import Rpi.GPIO as gpio
+
+import time
+
+
+gpio.setmode(gpio.BOARD)
+
+gpio.setup(14, gpio.OUT) #DIR1
+
+gpio.setup(1, gpio.OUT) #PWM1
+
+gpio.output(14, True)
+
+gpio.pwm(12, 50)
+
+time.sleep(60)
+
+gpio.cleanup()
