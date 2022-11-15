88e63798a7e38a47ebf762cce3ee31e79e27e860
Erik M <hans_kloss2@t-online.de>
2022-11-15 12:41:23 +0100

    sdfsdfg

--- Website for pi4 car/app.py
+++ Website for pi4 car/app.py
@@ -62,7 +62,7 @@ def UP():
     gpio.output(B_R_D, backward)
     brf = gpio.PWM(B_R, speed)
     brf.start(80)
-    time.sleep(2)
+    time.sleep(0.2)
 
 def DOWN():
     # Upper Left Forward
@@ -84,7 +84,7 @@ def DOWN():
     gpio.output(B_R_D, forward)
     brf = gpio.PWM(B_R, speed)
     brf.start(80)
-    time.sleep(2)
+    time.sleep(0.2)
 
 def LEFT():
     # Upper Left Forward
@@ -106,7 +106,7 @@ def LEFT():
     gpio.output(B_R_D, backward)
     brf = gpio.PWM(B_R, speed)
     brf.start(80)
-    time.sleep(2)
+    time.sleep(0.2)
 
 def RIGHT():
     # Upper Left Forward
@@ -128,7 +128,7 @@ def RIGHT():
     gpio.output(B_R_D, forward)
     brf = gpio.PWM(B_R, speed)
     brf.start(80)
-    time.sleep(2)
+    time.sleep(0.2)
 
 @app.route("/")
 
