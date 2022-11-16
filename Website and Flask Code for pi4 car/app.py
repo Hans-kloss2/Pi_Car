14cd6a4b81af73364e36035e97fb9a34762d3c35
Erik M <hans_kloss2@t-online.de>
2022-11-16 10:37:21 +0100

    asdasd

--- Website and Flask Code for pi4 car/app.py
+++ Website and Flask Code for pi4 car/app.py
@@ -151,16 +151,6 @@ def STOP():
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == 'POST':
-        gpio.setup(23, gpio.OUT)  # DIR1
-        gpio.setup(32, gpio.OUT)  # PWM1
-        gpio.setup(26, gpio.OUT)  # DIR2
-        gpio.setup(33, gpio.OUT)  # PWM2
-        gpio.setup(24, gpio.OUT)  # DIR1
-        gpio.setup(15, gpio.OUT)  # PWM1
-        gpio.setup(21, gpio.OUT)  # DIR2
-        gpio.setup(13, gpio.OUT)  # PWM2
-
-
         if request.form['submit_button'] == 'up':
             UP()
         elif request.form['submit_button'] == 'down':
