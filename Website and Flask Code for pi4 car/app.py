c7d732e6ea39b5c95e159b6fc8e39e1289248c53
Erik M <hans_kloss2@t-online.de>
2022-11-16 10:23:38 +0100

    asdadsf

--- Website and Flask Code for pi4 car/app.py
+++ Website and Flask Code for pi4 car/app.py
@@ -152,105 +152,90 @@ def STOP():
 def index():
     if request.method == 'POST':
         if request.form['submit_button'] == 'up':
-                # Upper Left Forward
-                gpio.output(U_L_D, forward)
-                ulf = gpio.PWM(U_L, speed)
-                ulf.start(80)
-
-                # Upper Right Forward
-                gpio.output(U_R_D, backward)
-                urf = gpio.PWM(U_R, speed)
-                urf.start(80)
-
-                # Bottom Left Forward
-                gpio.output(B_L_D, backward)
-                blf = gpio.PWM(B_L, speed)
-                blf.start(80)
-
-                # Bottom Right Forward
-                gpio.output(B_R_D, backward)
-                brf = gpio.PWM(B_R, speed)
-                brf.start(80)
+            # Upper Left Forward
+            gpio.output(U_L_D, forward)
+            ulf = gpio.PWM(U_L, speed)
+            ulf.start(80)
+            # Upper Right Forward
+            gpio.output(U_R_D, backward)
+            urf = gpio.PWM(U_R, speed)
+            urf.start(80)
+            # Bottom Left Forward
+            gpio.output(B_L_D, backward)
+            blf = gpio.PWM(B_L, speed)
+            blf.start(80)
+            # Bottom Right Forward
+            gpio.output(B_R_D, backward)
+            brf = gpio.PWM(B_R, speed)
+            brf.start(80)
         elif request.form['submit_button'] == 'down':
-                # Upper Left Forward
-                gpio.output(U_L_D, backward)
-                ulf = gpio.PWM(U_L, speed)
-                ulf.start(80)
-
-                # Upper Right Forward
-                gpio.output(U_R_D, forward)
-                urf = gpio.PWM(U_R, speed)
-                urf.start(80)
-
-                # Bottom Left Forward
-                gpio.output(B_L_D, forward)
-                blf = gpio.PWM(B_L, speed)
-                blf.start(80)
-
-                # Bottom Right Forward
-                gpio.output(B_R_D, forward)
-                brf = gpio.PWM(B_R, speed)
-                brf.start(80)
+            # Upper Left Forward
+            gpio.output(U_L_D, backward)
+            ulf = gpio.PWM(U_L, speed)
+            ulf.start(80)
+            # Upper Right Forward
+            gpio.output(U_R_D, forward)
+            urf = gpio.PWM(U_R, speed)
+            urf.start(80)
+            # Bottom Left Forward
+            gpio.output(B_L_D, forward)
+            blf = gpio.PWM(B_L, speed)
+            blf.start(80)
+            # Bottom Right Forward
+            gpio.output(B_R_D, forward)
+            brf = gpio.PWM(B_R, speed)
+            brf.start(80)
         elif request.form['submit_button'] == 'left':
-                # Upper Left Forward
-                gpio.output(U_L_D, backward)
-                ulf = gpio.PWM(U_L, speed)
-                ulf.start(80)
-
-                # Upper Right Forward
-                gpio.output(U_R_D, backward)
-                urf = gpio.PWM(U_R, speed)
-                urf.start(80)
-
-                # Bottom Left Forward
-                gpio.output(B_L_D, forward)
-                blf = gpio.PWM(B_L, speed)
-                blf.start(80)
-
-                # Bottom Right Forward
-                gpio.output(B_R_D, backward)
-                brf = gpio.PWM(B_R, speed)
-                brf.start(80)
+            # Upper Left Forward
+            gpio.output(U_L_D, backward)
+            ulf = gpio.PWM(U_L, speed)
+            ulf.start(80)
+            # Upper Right Forward
+            gpio.output(U_R_D, backward)
+            urf = gpio.PWM(U_R, speed)
+            urf.start(80)
+            # Bottom Left Forward
+            gpio.output(B_L_D, forward)
+            blf = gpio.PWM(B_L, speed)
+            blf.start(80)
+            # Bottom Right Forward
+            gpio.output(B_R_D, backward)
+            brf = gpio.PWM(B_R, speed)
+            brf.start(80)
         elif request.form['submit_button'] == 'right':
-                # Upper Left Forward
-                gpio.output(U_L_D, forward)
-                ulf = gpio.PWM(U_L, speed)
-                ulf.start(80)
-
-                # Upper Right Forward
-                gpio.output(U_R_D, forward)
-                urf = gpio.PWM(U_R, speed)
-                urf.start(80)
-
-                # Bottom Left Forward
-                gpio.output(B_L_D, backward)
-                blf = gpio.PWM(B_L, speed)
-                blf.start(80)
-
-                # Bottom Right Forward
-                gpio.output(B_R_D, forward)
-                brf = gpio.PWM(B_R, speed)
-                brf.start(80)
+            # Upper Left Forward
+            gpio.output(U_L_D, forward)
+            ulf = gpio.PWM(U_L, speed)
+            ulf.start(80)
+            # Upper Right Forward
+            gpio.output(U_R_D, forward)
+            urf = gpio.PWM(U_R, speed)
+            urf.start(80)
+            # Bottom Left Forward
+            gpio.output(B_L_D, backward)
+            blf = gpio.PWM(B_L, speed)
+            blf.start(80)
+            # Bottom Right Forward
+            gpio.output(B_R_D, forward)
+            brf = gpio.PWM(B_R, speed)
+            brf.start(80)
         elif request.form['submit_button'] == 'stop':
-                # Upper Left Forward
-                gpio.output(U_L_D, backward)
-                ulf = gpio.PWM(U_L, 0)
-                ulf.start(0)
-
-                # Upper Right Forward
-                gpio.output(U_R_D, backward)
-                urf = gpio.PWM(U_R, 0)
-                urf.start(0)
-
-                # Bottom Left Forward
-                gpio.output(B_L_D, backward)
-                blf = gpio.PWM(B_L, 0)
-                blf.start(0)
-
-                # Bottom Right Forward
-                gpio.output(B_R_D, backward)
-                brf = gpio.PWM(B_R, 0)
-                brf.start(0)
+            # Upper Left Forward
+            gpio.output(U_L_D, backward)
+            ulf = gpio.PWM(U_L, 0)
+            ulf.start(0)
+            # Upper Right Forward
+            gpio.output(U_R_D, backward)
+            urf = gpio.PWM(U_R, 0)
+            urf.start(0)
+            # Bottom Left Forward
+            gpio.output(B_L_D, backward)
+            blf = gpio.PWM(B_L, 0)
+            blf.start(0)
+            # Bottom Right Forward
+            gpio.output(B_R_D, backward)
+            brf = gpio.PWM(B_R, 0)
+            brf.start(0)
     return render_template('index.html')
 
 
