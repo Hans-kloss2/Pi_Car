412e4c67c6e60c50765e369530fdfa5a69955924
Erik M <51079728+Hans-kloss2@users.noreply.github.com>
2023-02-16 09:53:20 +0100

    Update app.py

--- Website and Python Code for pi4 car/app.py
+++ Website and Python Code for pi4 car/app.py
@@ -139,10 +139,9 @@ def STOP():
 def STILLLEFT():
     gpio.output(U_L_D, forward)
     gpio.output(U_L, forward)
-    # Bottom Left Forward
-    gpio.output(B_L_D, backward)
-    gpio.output(B_L, forward)
 
+    gpio.output(B_L_D, forward)
+    gpio.output(B_L, forward)
 
 # Driving right without rotating, hoepfully
 def STILLRIGHT():
