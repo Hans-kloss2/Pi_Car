377de7b6e08501479f3b306b3e17f40e0d469de7
Erik M <51079728+Hans-kloss2@users.noreply.github.com>
2023-02-16 09:34:39 +0100

    Update app.py

--- Website and Python Code for pi4 car/app.py
+++ Website and Python Code for pi4 car/app.py
@@ -141,24 +141,6 @@ def STILLLEFT():
     gpio.output(U_L, forward)
 
     # Upper Right Forward
-    gpio.output(U_R_D, backward)
-    gpio.output(U_R, forward)
-
-    # Bottom Left Forward
-    gpio.output(B_L_D, backward)
-    gpio.output(B_L, forward)
-
-    # Bottom Right Forward
-    gpio.output(B_R_D, forward)
-    gpio.output(B_R, forward)
-
-
-# Driving right without rotating, hoepfully
-def STILLRIGHT():
-    gpio.output(U_L_D, backward)
-    gpio.output(U_L, forward)
-
-    # Upper Right Forward
     gpio.output(U_R_D, forward)
     gpio.output(U_R, forward)
 
@@ -169,6 +151,24 @@ def STILLRIGHT():
     # Bottom Right Forward
     gpio.output(B_R_D, backward)
     gpio.output(B_R, forward)
+
+
+# Driving right without rotating, hoepfully
+def STILLRIGHT():
+    gpio.output(U_L_D, backward)
+    gpio.output(U_L, backward)
+
+    # Upper Right backward
+    gpio.output(U_R_D, backward)
+    gpio.output(U_R, backward)
+
+    # Bottom Left backward
+    gpio.output(B_L_D, backward)
+    gpio.output(B_L, backward)
+
+    # Bottom Right backward
+    gpio.output(B_R_D, forward)
+    gpio.output(B_R, backward)
 
 
 
