082a40a18725b087d54f359beedbb1be72481df8
Erik M <51079728+Hans-kloss2@users.noreply.github.com>
2023-04-13 10:14:44 +0200

    s

--- Bluetooth Control/server.py
+++ Bluetooth Control/server.py
@@ -7,15 +7,16 @@ JOYSTICK_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 # Get the MAC address of the ESP32
 esp32_address = "A8:03:2A:13:01:B2"  # Replace with the MAC address of the ESP32
 
+
+# Create a Bluetooth socket and connect to the ESP32
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((esp32_address, 1))
+
+# Send a command to read the joystick values
+command = bytes([0x01])
+sock.send(command)
+
 while True:
-    # Create a Bluetooth socket and connect to the ESP32
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    sock.connect((esp32_address, 1))
-
-    # Send a command to read the joystick values
-    command = bytes([0x01])
-    sock.send(command)
-
     # Receive the joystick values from the ESP32
     data = sock.recv(1024)
     print(f"Joystick values: {data}")
