6de0c5e34bad8ab421744849131e5d1a35628119
Erik M <51079728+Hans-kloss2@users.noreply.github.com>
2023-04-13 10:13:08 +0200

    s

--- Bluetooth Control/Arduino_BT_Control/Arduino_BT_Control.ino
+++ Bluetooth Control/Arduino_BT_Control/Arduino_BT_Control.ino
@@ -2,12 +2,12 @@
 
 // Define the address of the Raspberry Pi 4
 String server_address = "E4:5F:01:C6:44:9B";  // Replace with your Raspberry Pi 4's Bluetooth address
-
+// AC:67:B2:35:BF:9A
 BluetoothSerial SerialBT;
 
 void setup() {
   // Start the SerialBT module
-  SerialBT.begin("ESP32");  // Set the name of the ESP32 Bluetooth device
+  SerialBT.begin("JoyStick_Controler");  // Set the name of the ESP32 Bluetooth device
 
   // Connect to the Raspberry Pi 4
   SerialBT.connect(server_address);
@@ -18,11 +18,10 @@ void setup() {
 
 void loop() {
   // Read some data from a sensor or input pin
-  int sensor_data = 420;  // Replace with your sensor reading code
+  int sensor_data = 69;  // Replace with your sensor reading code
 
   // Format the data as a string
   String data = String(sensor_data);
-
   // Send the data over Bluetooth
   SerialBT.print(data);
 
